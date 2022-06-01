9487dd9ab2d7a269c50ce57a9fe3e27bdb4f405b
Elijah <bundielly@gmail.com>
2022-06-01 17:06:52 +0300

    Personal notes

---
+++ index.js
@@ -0,0 +1,98 @@
+/*
+const firstNumber = 32
+const secondNumber = 9
+const thirdNumber = 14
+const fourthNumber = 33
+const fifthNumber = 48
+const powerBall = 5
+
+function logWinningNumbers(first,second,third,fourth,fifth,power) {
+    console.log("Winning numbers:", first, second, third, fourth, fifth, power)
+}
+
+logWinningNumbers(
+    firstNumber,
+    secondNumber,
+    thirdNumber,
+    fourthNumber,
+    fifthNumber,
+    powerBall
+)
+*/
+/*
+const winningNumbers = [32,9,14,33,48,5]
+function logWinningNumbers(numbers) {
+    console.log("Winning numbers:", numbers)
+}
+logWinningNumbers(winningNumbers)
+console.log("Winning Number:",winningNumbers[3])
+*/
+/*
+const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+]
+console.log(alphabet.length)
+console.log(alphabet[alphabet.length -1])
+console.log(alphabet[-1])
+console.log(alphabet)
+console.log(alphabet[9] = "j")
+console.log(alphabet)
+*/
+/*
+const seriouslyNestedArray = [
+    "How",
+    ["deep", ["can", ["we", ["go", ["?"],"pretty"],"dang"],"deep"],"it"],
+    "seems."
+]
+console.log(seriouslyNestedArray[0])
+console.log(seriouslyNestedArray[1])
+console.log(seriouslyNestedArray[1][2])
+console.log(seriouslyNestedArray[1][1][1][1])
+console.log(seriouslyNestedArray[2])
+*/
+
+const board = [
+    ["x", "o", " "],
+    [" ", "x", "o"],
+    ["x", " ", "o"],
+]
+console.log(board)
+console.log(board[0][0])
+console.log(board[2][1])
+
+
+
+
+
+
+
+
+
+
+
+
+
